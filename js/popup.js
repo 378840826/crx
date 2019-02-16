dd6c9c0a9569f0c110e8744b09b6954da606050c
xie1206 <378840826@qq.com>
2019-02-16 17:49:11 +0800

    自动获取商品

--- js/popup.js
+++ js/popup.js
@@ -23,8 +23,9 @@ popupBtn.addEventListener('click', function() {
         atid: Number(atid),
         deadline: deadline,
     }, function(response) {
-        console.log("来自 content 的回复：" + response)
+        console.log("来自 content user 的回复：" + response)
     })
+    // 改变开关状态
     if (popupBtn.dataset.value == 'start') {
         popupBtn.dataset.value = 'end'
         popupBtn.innerText = '停止'
@@ -39,7 +40,7 @@ popupBtn.addEventListener('click', function() {
 sendMessageToContentScript({
     state: 'getState',
     }, function(response) {
-        console.log('打开popup时收到的消息', response);
+        console.log('打开popup时 请求 getState 收到的消息', response);
     // 设置按钮的属性
     if (response == 'start') {
         popupBtn.dataset.value = 'end'
@@ -49,3 +50,61 @@ sendMessageToContentScript({
         popupBtn.innerText = '开始'
     }
 })
+
+// 向 user 发送消息，获取当前抢购信息
+sendMessageToContentScript({
+    state: 'getGoodsInfo',
+    }, function(response) {
+        console.log('打开popup时 请求 getGoodsInfo 时收到的消息', response);
+        // 得到商品信息后，放到页面，提供选择
+        var container = document.querySelector('.div-goods-container')
+        var html = createGoods(response)
+        container.innerHTML = html
+})
+
+// 创建商品html
+var createGoods = function(goodsInfoArr) {
+    var html = ''
+    for (var i = 0; i < goodsInfoArr.length; i++) {
+        var goodsInfo = goodsInfoArr[i]
+        html += `
+            <span class="span-goods">
+                <div class="goods-title">${goodsInfo.title}</div>
+                <div class="goods-img">
+                    <img src="${goodsInfo.imgSrc}">
+                </div>
+                <div class="goods-number">
+                    <span class="span-goods-gid">
+                        gid: <span class="goods-gid">${goodsInfo.gid}</span>
+                    </span>
+                    <span class="span-goods-atid">
+                        atid: <span class="goods-atid">${goodsInfo.atid}</span>
+                    </span>
+                    <span class="span-deadline">
+                        抢购时间: <span class="goods-deadline">${goodsInfo.deadline}</span>
+                    </span>
+                </div>
+            </span>`
+    }
+    return html
+}
+
+// 商品点击自动填充
+var container = document.querySelector('.div-goods-container')
+container.addEventListener('click', function(event) {
+    var target = event.target
+    var goods = target.closest('.span-goods')
+    var gid = goods.querySelector('.goods-gid').innerText
+    var atid = goods.querySelector('.goods-atid').innerText
+    var deadline = goods.querySelector('.goods-deadline').innerText
+    document.querySelector('#id-gid').value = gid
+    document.querySelector('#id-atid').value = atid
+    document.querySelector('#id-deadline').value = deadline
+})
+
+
+
+
+
+
+//
--- js/user.js
+++ js/user.js
@@ -25,5 +25,66 @@ chrome.extension.onMessage.addListener(
             var state = document.body.dataset.state
             console.log('收到popup查询状态的请求', state);
             sendResponse(state)
+        } else if (request.state == 'getGoodsInfo') {
+            getNowGoodsIfon().then(function(goodsInfo) {
+                sendResponse(goodsInfo)
+            })
         }
 })
+
+// 获取即将抢购的商品的信息
+var getNowGoodsIfon = function() {
+    var date = new Date()
+    var nowHours = date.getHours()
+    var time = "10:00"
+    if (nowHours < 10) {
+        time = "10:00"
+    } else if (nowHours >= 10 && nowHours < 11) {
+        time = "11:00"
+    } else if (nowHours >= 11 && nowHours < 12) {
+        time = "12:00"
+    } else if (nowHours >= 12 && nowHours < 14) {
+        time = "14:00"
+    } else if (nowHours >= 14 && nowHours < 16) {
+        time = "16:00"
+    } else if (nowHours >= 16 && nowHours < 18) {
+        time = "18:00"
+    } else if (nowHours >= 18 && nowHours < 20) {
+        time = "20:00"
+    } else if (nowHours >= 20 && nowHours < 22) {
+        time = "22:00"
+    }
+    var deadline = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${time}:00`
+    var promise = new Promise(function(resolve, reject) {
+        $.ajax({
+            type : 'post',
+            url: '/share/activitys/suprise.html?do=getNewSurprise',
+            data: {time},
+            async : false,
+            success: function(res) {
+                var div = document.createElement('ul')
+                div.innerHTML = res
+                var liAll = div.querySelectorAll('li')
+                var goodsArr = []
+                for (var i = 0; i < liAll.length; i++) {
+                    var li = liAll[i]
+                    // 找出 1 折的商品
+                    if (li.querySelector('.cut1')) {
+                        var goods = {
+                            title: li.querySelector('.goodsDes').innerText,
+                            imgSrc: li.querySelector('img').src,
+                            gid: Number(li.querySelector('.gid').value),
+                            atid: Number(li.querySelector('.atid').value),
+                            deadline: deadline,
+                        }
+                        goodsArr.push(goods)
+                    }
+                }
+
+                var goodsInfo = goodsArr
+                resolve(goodsInfo)
+            },
+        })
+    })
+    return promise
+}
