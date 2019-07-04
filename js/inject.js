38d20fa779250fbf2ff959b60bf589f4a4c8117a
xie1206 <378840826@qq.com>
2019-07-04 11:09:02 +0800

    atid不能提前获取，改版4.0

--- js/inject.js
+++ js/inject.js
@@ -73,13 +73,10 @@ const addGoodsTocart = function (goodsInfo, url) {
             // 延迟关闭请求
             setTimeout(function() {
                 window.clearInterval(berserkTimer)
-            }, 100)
-        } else if (res.includes('抢购上限')) {
-            console.log('%c 抢到了，15分钟内去付款！','background:#ccc;color:#f00;font-size:20px;');
-            // 延迟关闭请求, 并跳转至购物车
-            setTimeout(function() {
-                window.clearInterval(berserkTimer)
-            }, 100)
+            }, 10000)
+        } else if (res.includes('<div>')) {
+            console.log('%c 抢到了，15分钟内去付款！！！！！！','background:#ccc;color:#f00;font-size:40px;');
+            window.clearInterval(berserkTimer)
         } else {
             console.log(res);
         }
@@ -89,6 +86,9 @@ const addGoodsTocart = function (goodsInfo, url) {
 // 循环加入购物车
 const addToCart = function (goodsInfo, options) {
     console.log('开始抢购')
+    if (window.berserkTimer) {
+        window.clearInterval(berserkTimer)
+    }
     // url 放这里是避免在循环执行的 addGoodsTocart 函数中做不必要的重复执行 reurl
     var url = reurl("share/ajax.html")
     berserkTimer =  setInterval(function () {
@@ -96,11 +96,13 @@ const addToCart = function (goodsInfo, options) {
     }, options.addCartLoopTimeMs)
 }
 
+// 时间监控
 const monitor = (goodsInfo, options) => {
-    console.log(`%c准备抢购：${goodsInfo.title}`, 'color:#f00;font-size:20px;');
-    console.log(`%c抢购时间为：${timeFormat(goodsInfo.deadline)}`, 'color:#0976cd;');
-    console.log(`%c提前 ${options.speedinessTime} 秒开始抢购`, 'color:#0976cd;');
-    console.log(`%c每秒抢购 ${options.frequency} 次`, 'color:#0976cd;');
+    console.log('goodsInfo', goodsInfo);
+    console.log(`%c名称：${goodsInfo.title}`, 'color:#f00;font-size:20px;');
+    console.log(`%c时间：${timeFormat(goodsInfo.deadline)}`, 'color:#0976cd;');
+    console.log(`%c提前： ${options.speedinessTime} 秒`, 'color:#0976cd;');
+    console.log(`%c每秒： ${options.frequency} 次`, 'color:#0976cd;');
     // 获取点击开启时的服务器时间和抢购时间
     let nowServiceTime = getServiceTime()
     let deadline = goodsInfo.deadline
@@ -113,14 +115,82 @@ const monitor = (goodsInfo, options) => {
     // 因为下面的计时是 1 秒后才打印，所以 i 的初始值设为 1000 ms，time_s 也要先 -1
     let i = 1000
     time_s = time_s
+    let time = `${goodsInfo.deadline.getHours()}:00`
     timer = setInterval(function() {
         // 每秒钟 剩余时间 -1
         time_s = time_s - 1
         // 如果快到时间了
         if (time_s <= options.speedinessTime) {
-            addToCart(goodsInfo, options)
             // 停止计时
             window.clearInterval(timer)
+            // 获取 atid（改版后之前获取的 atid 是错误的）
+            // atid 有规律时，可以手动在页面填，如果没填才请求
+            if (goodsInfo.atid == '0') {
+                getAtidTime = setInterval(function() {
+                    $.ajax({
+                        type : 'post',
+                        url: '/share/activitys/suprise.html?do=getNewSurprise',
+                        data: {time},
+                        success: function(res) {
+                            // 判断是否获取到 atid （时间到了才能获取）
+                            if (res.slice(0, 2000).includes(`class="atid" value="0"`)) {
+                                console.log('atid 为 0， 获取失败');
+                                return
+                            } else {
+                                let gidIndex_1 = res.indexOf(`class="gid" value="`)
+                                let gid_1 = res.slice(gidIndex_1 + 19, gidIndex_1 + 19 + 6)
+                                let atidIndex_1 = res.indexOf(`class="atid" value="`)
+                                let atid_1 = res.slice(atidIndex_1 + 20, atidIndex_1 + 20 + 6)
+                                if (gid_1 == goodsInfo.gid) {
+                                    goodsInfo.atid = atid_1
+                                    // 加购物车
+                                    addToCart(goodsInfo, options)
+                                } else {
+                                    res = res.slice(atidIndex_1 + 20 + 6)
+                                    let atidIndex_2 = res.indexOf(`class="atid" value="`)
+                                    let atid_2 = res.slice(atidIndex_2 + 20, atidIndex_2 + 20 + 6)
+                                    goodsInfo.atid = atid_1
+                                    // 加购物车
+                                    addToCart(goodsInfo, options)
+                                }
+                                // 停止获取 atid
+                                window.clearInterval(getAtidTime)
+                            }
+
+                            // let div = document.createElement('ul')
+                            // div.innerHTML = res
+                            // // 判断是否获取到了 atid
+                            // if (div.getElementsByClassName('atid')[0].value === '0') {
+                            //     console.log('atid 为 0， 获取失败');
+                            //     return
+                            // }
+                            // let liAll = div.querySelectorAll('li')
+                            // let goodsArr = []
+                            // // 找出商品的关键信息(前 2 个为 1折商品)
+                            // for (let i = 0; i < 2; i++) {
+                            //     let li = liAll[i]
+                            //     var gid = li.getElementsByClassName('gid')[0].value
+                            //     if (gid == goodsInfo.gid) {
+                            //         goodsInfo.atid = li.getElementsByClassName('atid')[0].value
+                            //         // 加购物车
+                            //         addToCart(goodsInfo, options)
+                            //         break
+                            //     }
+                            // }
+                            // console.log('如果未开始抢购 则程序判断错误');
+                            // // 停止获取 atid
+                            // window.clearInterval(getAtidTime)
+                        },
+                        error: function(err) {
+                            console.log('请求折扣商品数据失败', err);
+                        },
+                    })
+                }, 5)
+            } else {
+                // 如果手动填了 atid
+                // 加购物车
+                addToCart(goodsInfo, options)
+            }
         } else {
             let nowTimestamp = new Date(nowServiceTime).getTime() + i
             i = i + 1000
